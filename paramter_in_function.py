ac2030cac90de127a295251c6980a5cb2a1163b5
Charles Arulanandu <111447307+Beingcharles@users.noreply.github.com>
2024-06-15 08:29:40 +0100

    Add files via upload

---
+++ paramter_in_function.py
@@ -0,0 +1,3 @@
+def TFL(msg):
+    print("message to TFL users: ",msg)
+TFL("mind the gap between the platforms!")
---
+++ score_percentage.py
@@ -0,0 +1,7 @@
+score_percentage=int(input("percentage: "))
+if(score_percentage>=70):
+    name=input("enter your name: ")
+    age=input("enter your age: ")
+    print("you're eligible")
+else:
+    print("you're not eligible")
