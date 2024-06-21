46403783c3475f2691a4e497497e0f128d83a180
Charles Arulanandu <111447307+Beingcharles@users.noreply.github.com>
2024-06-21 12:46:12 +0100

    Add files via upload

---
+++ class_and_objects_eg4.py
@@ -0,0 +1,30 @@
+class student:
+    def __init_(self):
+        self.name=""
+        self.register_number=""
+    def display(self):
+        print("name: ",self.name)
+        print("reg.no: ",self.register_number)
+s1=student()
+s2=student()
+s3=student()
+s4=student()
+s5=student()
+
+s1.name="charles"
+s1.register_number="21540720"
+s2.name="balaji"
+s2.register_number="22640721"
+s3.name="chandru"
+s3.register_number="23740722"
+s4.name="dinesh"
+s4.register_number="24840723"
+s5.name="ebin"
+s5.register_number="25940724"
+
+
+s1.display()
+s2.display()
+s3.display()
+s4.display()
+s5.display()
