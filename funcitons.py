d8b904ef4335ebca38f1d25b47b5abb68cbc09e9
Charles Arulanandu <111447307+Beingcharles@users.noreply.github.com>
2024-06-25 11:44:26 +0100

    Add files via upload

---
+++ funcitons.py
@@ -0,0 +1,29 @@
+def sub():
+    print("subraction: ")
+    a=int(input("enter a: "))
+    b=int(input("enter b: "))
+    print(a-b)
+
+def mod():
+    print("modulus: ")
+    c=int(input("enter c: "))
+    d=int(input("enter d: "))
+    print(c%d)
+
+def exp():
+    print("exponential: ")
+    e=int(input("enter e: "))
+    f=int(input("enter f: "))
+    print(e**f)
+
+def mul():
+    print("multiplication: ")
+    g=int(input("enter g: "))
+    h=int(input("enter h: "))
+    print(g*h)
+
+    
+sub()
+mod()
+exp()
+mul()
